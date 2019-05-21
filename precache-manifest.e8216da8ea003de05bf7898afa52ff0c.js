3a2b1312af7a133c1384ee2fd3e47d1c5d154a5b
brnkimani <brnkimani@gmail.com>
2019-05-21 23:42:10 +0300

    Updates

---
+++ precache-manifest.e8216da8ea003de05bf7898afa52ff0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b72dbaccb6817c46df93a7c2d75faa9",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "bd6f5356fd0757896b65",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "373a86af239bad5ce213",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "bd6f5356fd0757896b65",
+    "url": "/contactmanager/static/js/2.0d46e735.chunk.js"
+  },
+  {
+    "revision": "373a86af239bad5ce213",
+    "url": "/contactmanager/static/js/main.3e01734e.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
